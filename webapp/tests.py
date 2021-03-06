c41604fbe4ed0c2895e13bab1884d84d90776b17
Maciej Dziardziel <fiedzia@gmail.com>
2021-03-06 21:35:45 +0000

    more tests

--- webapp/tests.py
+++ webapp/tests.py
@@ -9,5 +9,16 @@ class RoutingTestCase(TestCase):
 
     def test_routing(self):
         client = Client()
-        resp = client.get('/route/', {'node_from': '1', 'node_to': '3'}))
-        self.assetEqual(json.loads(resp.content), [1, 2, 3])
+        resp = client.get('/route/', {'node_from': '1', 'node_to': '3'})
+        self.assertEqual(json.loads(resp.content), [1, 2, 3])
+
+    def test_routing_same_node(self):
+        client = Client()
+        resp = client.get('/route/', {'node_from': '1', 'node_to': '1'})
+        self.assertEqual(json.loads(resp.content), [1])
+
+    def test_routing_invalid_node(self):
+        client = Client()
+        resp = client.get('/route/', {'node_from': '1', 'node_to': '5'})
+        self.assertEqual(resp.status_code, 400)
+
